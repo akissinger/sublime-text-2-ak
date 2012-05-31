7af566b6e7efd082d32d00b339e022d513274e40
Aleks Kissinger <aleks0@gmail.com>
2012-06-01 00:31:17 +0100

    pyside test command

---
+++ test_pyside.py
@@ -0,0 +1,36 @@
+import sublime, sublime_plugin
+import sys
+
+sys.path.append('/usr/lib/python2.7/dist-packages')
+
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+class OutputBox(QDialog):
+	def __init__(self,parent=None):
+		super(OutputBox,self).__init__(parent)
+		self.setWindowTitle("Snarf")
+		self.edit = QLineEdit("Input stuff")
+		self.button = QPushButton("Then Punch")
+		layout = QVBoxLayout()
+		layout.addWidget(self.edit)
+		layout.addWidget(self.button)
+		self.setLayout(layout)
+
+		self.button.clicked.connect(self.display_message)
+
+	def display_message(self):
+		sublime.status_message("Bam: %s" % self.edit.text())
+
+class TestPysideCommand(sublime_plugin.WindowCommand):
+	def open_window(self):
+		if QApplication.instance() == None:
+			QApplication(['-sublime-'])
+		win = OutputBox()
+		win.show()
+		QApplication.instance().exec_()
+
+	def run(self):
+		print "test"
+		self.open_window()
+		print "finished open_window"
